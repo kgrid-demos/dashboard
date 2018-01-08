bcf4d41d580047d14d021c2006c31f58b64e9a04
George Meng <gqmeng@umich.edu>
2018-01-08 11:07:41 -0500

    Limited the moment.js locale to de|en for dev

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -23,6 +23,7 @@ module.exports = merge(baseWebpackConfig, {
       'process.env': config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|en/),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
